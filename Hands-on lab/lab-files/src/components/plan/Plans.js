79d2b14312f3cd5132d831c87bb8e982f72c5cd3
Kader Derk <kaderderk@gmail.com>
2020-09-20 22:41:43 -0500

    updated index file

--- Hands-on lab/lab-files/src/components/plan/Plans.js
+++ Hands-on lab/lab-files/src/components/plan/Plans.js
@@ -58,6 +58,7 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
+                <h3>Welcome to Best For You best organic Product</h3>
                 <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
